b1f563a4d3abf16c0c2f132260853d69a689be28
S19102002 <139078240+S19102002@users.noreply.github.com>
2026-06-09 18:17:05 +0530

    Create RegistrationModel.cs

---
+++ RegistrationModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
